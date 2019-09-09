8b087f5dc7f355bebd57f21b2219b748638ee2be
DerBeton <32222199+DerBeton@users.noreply.github.com>
2019-09-09 22:59:19 +0200

    Working on version.php (not working)

--- plexAskWeb/admin/version.php
+++ plexAskWeb/admin/version.php
@@ -1,12 +1,44 @@
 <?php
 
   $config = json_decode(file_get_contents("../data/config.json"), true);
+  $localVersion = json_decode(file_get_contents("../version.json"), true);
 
-  if (isset($_POST["token"]) && $_POST['token'] == $config['admin']['token']) {
+  if (isset($_GET["token"]) && $_GET['token'] == $config['admin']['token']) {
 
     $response = array();
 
-    $version = $config['version']['number'];
+    $version = $localVersion['version']['number'];
+
+    // context for github api otherwise request is rejected
+    $opts = [
+            'http' => [
+                    'method' => 'GET',
+                    'header' => [
+                            'User-Agent: PHP'
+                    ]
+            ]
+    ];
+
+    $context = stream_context_create($opts);
+
+    $remoteVersion = json_decode(file_get_contents("https://api.github.com/repos/DerBeton/PlexAsk/git/trees/update-process", true, $context));
+
+    //https://api.github.com/repos/DerBeton/PlexAsk/git/trees/update-process
+
+    foreach ($remoteVersion as $key) {
+
+      print_r($key);
+
+      // code...
+      echo $key['path'];
+
+    }
+
+
+    echo "</br>";
+
+    //print_r($config);
+    //echo $remoteVersion[''];
 
     $versions = json_decode(file_get_contents("https://updateplex.derbeton.ch/versions.json"), true);
 
@@ -17,7 +49,7 @@
     if(version_compare($curVersion, $version, ">")) {
 
 
-      file_put_contents("../updatever.php", fopen("https://updateplex.derbeton.ch/updatever.php.dat", 'r'));
+      //file_put_contents("../updatever.php", fopen("https://updateplex.derbeton.ch/updatever.php.dat", 'r'));
 
 
       $response['status'] = 'update';
