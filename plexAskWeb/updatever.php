cafa48c2575fbae9ac85be5f2eb7b7677252ac2b
DerBeton <32222199+DerBeton@users.noreply.github.com>
2019-09-11 22:26:40 +0200

    Updated updatever.php to show errors and create zips also under windows

--- plexAskWeb/updatever.php
+++ plexAskWeb/updatever.php
@@ -1,17 +1,18 @@
 <?php
-
 	// set default timezone
 	date_default_timezone_set('Europe/Zurich');
 
 	// get token from config
 	$config = json_decode(file_get_contents("data/config.json"), true);
 
+	global $updateStatus;
+
 	// compare token else go back to main site
 	if (isset($_GET["t"]) && $_GET['t'] == $config['admin']['token']) {
-		
+
 		// create backup zip in backup folder
 		createZip("./"); // pass path
-		
+
 		// context for github api otherwise request is rejected
 		$opts = [
 						'http' => [
@@ -22,24 +23,50 @@
 						]
 		];
 		$context = stream_context_create($opts);
+
 		$remoteRepo = json_decode(file_get_contents("https://api.github.com/repos/DerBeton/PlexAsk/git/trees/update-process", false, $context));
 
-		// get plexAskWeb folder url on github api
-		foreach($remoteRepo->tree as $key=>$value) {
+		if ($remoteRepo === true) {
 
-			if($value->path == "plexAskWeb") {
-				$mainUrl = $value->url;
-				getFiles($mainUrl, $context, "");
-				break;
+			// get plexAskWeb folder url on github api
+			foreach($remoteRepo->tree as $key=>$value) {
+
+				if($value->path == "plexAskWeb") {
+					$mainUrl = $value->url;
+					getFiles($mainUrl, $context, "");
+					break;
+				}
+
 			}
 
+		} else {
+
+			$updateStatus = "api";
+
 		}
-		
-		// when update is finished, go to main site with update = true
-		$config['version']['number'] = $_GET['ver'];
-		file_put_contents("data/config.json",json_encode($config));
-		
-		header("location: request?update=true");
+
+
+			switch($updateStatus){
+
+				case "success":
+					// when update is finished
+					$config['version']['number'] = $_GET['ver'];
+					file_put_contents("data/config.json",json_encode($config));
+					echo "Update erfolgreich durchgeführt <a href='request/'>Zurück zur Startseite</a>";
+					break;
+
+				case "api":
+					echo "<h2 style='color: red;'>GitHub Api Limit erreicht (in 1h neu versuchen oder mit GitHub account in Browser anmelden und nochmals probieren...) <a href='https://developer.github.com/v3/#rate-limiting'>GitHub Dokumentation</a></h2>";
+					break;
+
+				case "zip":
+					echo "<h2 style='color: red;'>Backup Zip konnte nicht erstellt werden :(</h2>";
+					break;
+
+				default:
+					echo "something unknown went wrong...";
+			}
+
 
 
 	} else {
@@ -49,7 +76,7 @@
 
 	}
 
-	
+
 	##################################################################################
 
 									// Erstelle ein zip Archive im backup ordner //
@@ -57,48 +84,55 @@
 	##################################################################################
 
 
-	function createZip($path) { 
+	function createZip($path) {
 
 		// if backup folder doesn't exist create it
 		if(!is_dir("backup")) {
 			mkdir("backup");
 		}
-		
+
 		// path and name of new zip file
-		$zipcreated = "./backup/backup-" . date('d-m-Y:H:i:s') . ".zip"; 
+		$zipcreated = "./backup/backup-". date('d-m-Y_H-i-s') . ".zip";
 
-		// Create new zip class 
-		$zip = new ZipArchive; 
+		// Create new zip class
+		$zip = new ZipArchive;
 
-		if($zip -> open($zipcreated, ZipArchive::CREATE ) === TRUE) { 
+		if($zip -> open($zipcreated, ZipArchive::CREATE ) === TRUE) {
 
 				// get all subfolders in path
 				$subdirs = array_filter(glob('*'), 'is_dir');
-			
-			
+
+
 				// go through array and save folders in $dir
 				foreach($subdirs as $dir) {
-					
+
 					// dont backup the backup folder
 					if ($dir == "backup") {
 						continue;
 					}
-					
+
 					// get the id of folder for readdir
-					$curdir = opendir($dir); 
-					
+					$curdir = opendir($dir);
+
 					// add every file in zip with subfolder and dont backup config.json (security reason)
-					while($file = readdir($curdir)) { 
-							if(is_file($path.$dir."/".$file) and $file != "config.json") { 
-									$zip -> addFile($path.$dir."/".$file, $dir."/".$file); 
+					while($file = readdir($curdir)) {
+							if(is_file($path.$dir."/".$file) and $file != "config.json") {
+									$zip -> addFile($path.$dir."/".$file, $dir."/".$file);
 							}
 					}
-					
+
 				}
-			
-				$zip ->close(); 
-		} 
-	
+
+				echo "Es wurde ein <span style='color: red;'>Backup</span> vom jetzigen Stand unter dem Ordner backup im root Verzeichnis erstellt. Bei Problemen kann dies zurückgespielt werden";
+				echo "</br>";
+				echo "</br>";
+
+				$zip ->close();
+		}
+
+		global $updateStatus;
+		$updateStatus = "zip";
+
 	} // end createZip
 
 	##################################################################################
@@ -109,57 +143,60 @@
 
 
 	function getFiles($mainUrl, $context, $path) {
-		
+
 		$remoteFiles = json_decode(file_get_contents($mainUrl, false, $context));
-		
+
+
 		foreach($remoteFiles->tree as $key=>$value) {
-			
+
 			if($value->type == "blob") {
-				
+
 				$file = $value->path;
 				$filePath = $path.$value->path;
 				if(file_exists($filePath)) {
 
-					$localFile = file_get_contents($filePath);					
-					
+					$localFile = file_get_contents($filePath);
+
 				} else {
-					
+
 					if(!is_dir($path)) {
 						mkdir($path);
-					}					
-					
+					}
+
 					$localFile = NULL;
-				
+
 				}
-				
+
 
 				// pfff need to compare hash with blob, size and \0 and than the filecontent because github uses the git hash-object function to hash object
 				if($value->sha != sha1("blob " . $value->size . "\0" . $localFile) and $file != "updatever.php" and $file != "films" and $file != "series" and $file != "config.json") {
-					
+
 					downloadFile($file, $path);
-					
+
 				} else {
-				
-					echo "File " . $file . " ist gleich wie auf github";
+
+					echo "File " . $file . " musste nicht upgedated werden";
 					echo "</br>";
-					
+					global $updateStatus;
+					$updateStatus = "success";
+
 				}
-			
+
 			} elseif($value->type == "tree") {
-				
+
 				$subUrl = $value->url;
 				$subPath = $path . $value->path . "/";
-				
+
 				getFiles($subUrl, $context, $subPath);
-				
-				
+
+
 				}
-		
+
 		}
-	
+
 	} // end getFiles
 
-	
+
 	##################################################################################
 
 											// download Files which were udpated //
@@ -167,13 +204,13 @@
 	##################################################################################
 
 	function downloadFile(String $file,String $path) {
-		
+
 		file_put_contents($path.$file, fopen("https://raw.githubusercontent.com/DerBeton/PlexAsk/update-process/plexAskWeb/".$path.$file, "r"));
-		
+
 		echo "File " . $file . " wurde auf den neusten Stand gebracht";
 		echo "</br>";
-	
-	
+
+
 	}
 
 
